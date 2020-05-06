bef1d1d6d703382dda96dfbf105f92df1cf33a33
martin-ghs <o68938529@126.com>
2020-05-06 15:23:19 +0800

    装饰器用的还不熟练

--- index_list.py
+++ index_list.py
@@ -1,8 +1,9 @@
 import random
+import time
 from sys import argv
 
 
-def num_list_generator(aug):
+def random_num_list_generator(aug):
     
     num_list = list()
 
@@ -15,25 +16,38 @@ def num_list_generator(aug):
 
 def main():
     
-    # 随机生成一个包含十个随机数的列表
-    aug = argv[1]
-    num_list = num_list_generator(aug)
-    print('生成的列表是:{}'.format(num_list))
-    
-    index_nums = input("输入数字：")
-    index_nums = index_nums.split(",")
-    
-    index_list = list()
-    
-    for i in index_nums:
-        index_list.append(int(i))
-    # print(index_list)
+    start_time = time.time()
 
-    for i in range(int(aug)):
-        if num_list[i] in index_list:
-            print('{}在生成的列表中，排序是{}'.format(num_list[i], i+1))
-        else:
-            continue
+    # 随机生成一个包含十个随机数的列表
+    try:
+        aug = argv[1]
+
+    except Exception:
+        print('错误：缺少必要的参数')
+        return
+    
+    else:
+        num_list = random_num_list_generator(aug)
+        print('生成的列表是:{}'.format(num_list))
+        
+        index_nums = input("输入数字：")
+        index_nums = index_nums.split(",")
+        
+        index_list = list()
+        
+        for i in index_nums:
+            index_list.append(int(i))
+        # print(index_list)
+
+        for i in range(int(aug)):
+            if num_list[i] in index_list:
+                print('{}存在于生成的列表中，序号是{}'.format(num_list[i], i+1))
+            else:
+                continue
+    over_time = time.time()
+
+    # print('时长：%d' % int(over_time))
+    print(over_time-start_time)
 
 
 if __name__ == '__main__':
