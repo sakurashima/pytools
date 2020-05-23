bb46e32eba080eb1404bb7be110b10875bef8a90
martin-ghs <o68938529@126.com>
2020-05-23 22:04:25 +0800

    使用ip代理

--- requests_exercise/use_proxise/proxies_01.py
+++ requests_exercise/use_proxise/proxies_01.py
@@ -3,6 +3,7 @@ import requests
 
 url = 'https://www.bilibili.com'
 
+
 headers = {
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
 }
