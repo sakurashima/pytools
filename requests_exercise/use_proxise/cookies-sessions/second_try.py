c59e4e662ac7f4c433024d0bb3c9875212baedda
sakurajima <57747971+saikurajima@users.noreply.github.com>
2020-05-24 11:43:56 +0800

    Update second_try.py

--- requests_exercise/use_proxise/cookies-sessions/second_try.py
+++ requests_exercise/use_proxise/cookies-sessions/second_try.py
@@ -4,7 +4,7 @@ import requests
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-    'Cookie':'DWRSESSIONID=TDAim5cygeNPHCJszeOcxNTD50n; JSESSIONID=D5AE358D2D76880EAD80E98835E1DB38.TM1',
+    'Cookie':'dshdjahdjadhjhdjahdjhadkjhjkdshjkahdjkha5E1DB38.TM1',
 }
 
 url = 'http://jxpt.ahu.edu.cn/meol/personal.do'
